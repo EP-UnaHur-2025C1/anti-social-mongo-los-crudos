44271d532d07d2eea5a678ff30fd02148f2ab85c
Crisdgomez <cristiandaniel.gomez@estudiantes.unahur.edu.ar>
2025-06-22 01:53:54 -0300

    se agrega swagger

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ const { userRoute,
         tagRoute,
         commentTagRoute } = require("./routes")
 require('dotenv').config()
+const swaggerUi = require("swagger-ui-express")
+const YAML = require("yamljs")
+const path = require("path")
+const swaggerDocument = YAML.load(path.join(__dirname, "../swagger.yaml"))
 const app = express()
 const PORT = process.env.PORT ?? 5050
 
@@ -17,6 +21,12 @@ app.use("/post-images", postImagesRoute)
 app.use("/comments", commentRoute)
 app.use("/tags", tagRoute)
 app.use("/comment-tags", commentTagRoute)
+
+// Configurar Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  customCss: '.swagger-ui .topbar { display: none }',
+  customSiteTitle: "Anti-Social Network API Documentation"
+}))
 
 app.listen(PORT, async (error) => {
   if (error) {
@@ -30,4 +40,5 @@ app.listen(PORT, async (error) => {
     process.exit(1)
   }
   console.log(`App iniciada en http://0.0.0.0:${PORT}`)
+  console.log(`Documentación API disponible en http://localhost:${PORT}/api-docs`)
 })
